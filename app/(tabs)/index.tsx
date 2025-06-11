fb4f0d568d4a0885d6e9040c977779d0c5e19a99
Even <evanjephi@gmail.com>
2025-06-10 23:29:54 -0400

    ch init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import 'react-native-gesture-handler';
 import { LineChart } from 'react-native-wagmi-charts';
 
 const API_KEY = Constants.expoConfig?.extra?.TWELVE_DATA_API_KEY || '';
