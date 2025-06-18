ef90bd1b7d537cb7d7ca4658ba79923e646527e2
Even <evanjephi@gmail.com>
2025-06-17 22:50:17 -0400

    created seperate style

---
+++ styles/adminPostsStyles.ts
@@ -0,0 +1,95 @@
+import { StyleSheet } from 'react-native';
+
+const adminPostsStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#F4F8FB',
+  },
+  header: {
+    fontSize: 28,
+    marginBottom: 4,
+    textAlign: 'center',
+    color: '#4F8EF7',
+    fontWeight: 'bold',
+  },
+  subHeader: {
+    fontSize: 16,
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#555',
+  },
+  formCard: {
+    backgroundColor: '#fff',
+    borderRadius: 14,
+    padding: 16,
+    marginBottom: 24,
+    shadowColor: '#4F8EF7',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#E0E7EF',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 12,
+    backgroundColor: '#F8FAFC',
+    fontSize: 16,
+    color: '#222',
+  },
+  postButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#4F8EF7',
+    borderRadius: 8,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    marginTop: 4,
+  },
+  postButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    marginLeft: 8,
+    fontSize: 16,
+  },
+  postsHeader: {
+    fontSize: 20,
+    marginBottom: 12,
+    color: '#4F8EF7',
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  postCard: {
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: '#4F8EF7',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.06,
+    shadowRadius: 6,
+    elevation: 1,
+  },
+  postTitle: {
+    fontSize: 18,
+    color: '#222',
+    fontWeight: '600',
+  },
+  postContent: {
+    fontSize: 16,
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 2,
+  },
+  postDate: {
+    fontSize: 12,
+    color: '#888',
+    marginTop: 6,
+    textAlign: 'right',
+  },
+});
+
+export default adminPostsStyles;
