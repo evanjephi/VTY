8a46daeb50b6f2713cd89b79e0c5a14d71f6d3a8
Even Yohans <73504127+evanjephi@users.noreply.github.com>
2025-08-14 23:18:12 -0400

    Update explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -7,7 +7,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-
+//tab two screen
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
